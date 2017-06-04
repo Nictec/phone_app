92f8705fc7b8920044a331eabaf9938b275bfd6e
Nictec <nicnac9911@gmail.com>
2017-06-04 13:47:26 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <!-- App -->
+  <div id="app">
+
+    <!-- Statusbar -->
+    <f7-statusbar></f7-statusbar>
+
+    <!-- Main Views -->
+    <f7-views layout="dark">
+      <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
+        <!-- Navbar -->
+        <f7-navbar>
+          <f7-nav-center sliding>EasyRent</f7-nav-center>
+        </f7-navbar>
+        <!-- Pages -->
+        <f7-pages>
+          <f7-page>
+          </f7-page>
+        </f7-pages>
+      </f7-view>
+    </f7-views>
+
+    <!-- Popup -->
+    <f7-popup id="popup">
+      <f7-view navbar-fixed>
+        <f7-pages>
+          <f7-page>
+            <f7-navbar title="Popup">
+              <f7-nav-right>
+                <f7-link close-popup>Close</f7-link>
+              </f7-nav-right>
+            </f7-navbar>
+            <f7-block>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.</f7-block>
+          </f7-page>
+        </f7-pages>
+      </f7-view>
+    </f7-popup>
+
+    <!-- Login Screen -->
+    <f7-login-screen id="login-screen">
+      <f7-view>
+        <f7-pages>
+          <f7-page login-screen>
+            <f7-login-screen-title>Login</f7-login-screen-title>
+            <f7-list form>
+              <f7-list-item>
+                <f7-label>Username</f7-label>
+                <f7-input name="username" placeholder="Username" type="text"></f7-input>
+              </f7-list-item>
+              <f7-list-item>
+                <f7-label>Password</f7-label>
+                <f7-input name="password" type="password" placeholder="Password"></f7-input>
+              </f7-list-item>
+            </f7-list>
+            <f7-list>
+              <f7-list-button title="Sign In" close-login-screen></f7-list-button>
+              <f7-list-label>
+                <p>Click Sign In to close Login Screen</p>
+              </f7-list-label>
+            </f7-list>
+          </f7-page>
+        </f7-pages>
+      </f7-view>
+    </f7-login-screen>
+
+    <!-- toolbar -->
+    <f7-toolbar>
+      <f7-link></f7-link>
+    </f7-toolbar>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
